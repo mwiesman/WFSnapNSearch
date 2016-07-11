93b19b8753821240a6abb79ef557d6bf66694414
tianchuliang <liangtianchu@gmail.com>
2016-07-10 21:35:46 -0400

    Revert "screenshots for devpost and more screenshots; edited gitignore; pushed train.sh files for two nets"
    This reverts commit 703cb225fe2df41f2dc38d591a10d60aa1a6795f.

--- neural_network/object_net/train.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-./build/tools/caffe train \
-    -solver object_net/solver.prototxt \
-    -weights models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
-    -gpu 0 
--- neural_network/scene_net/train.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-./build/tools/caffe train \
-    -solver scene_net/scene_net/solver.prototxt \
-    -weights models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel \
-    -gpu 0
